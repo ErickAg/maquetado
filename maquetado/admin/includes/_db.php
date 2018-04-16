7206d4bf23994464ad19caaaf06a3d86b7d2eb86
Erick Alor <erickalor.92@gmail.com>
2018-04-16 11:36:09 -0500

    Add files via upload

--- maquetado/admin/includes/_db.php
+++ maquetado/admin/includes/_db.php
@@ -4,9 +4,9 @@ use Medoo\Medoo;
 global $db;
 $db = new Medoo([
 	'database_type' => 'mysql',
-	'database_name' => 'tecnol43_servicios',
+	'database_name' => 'tecnol43_principal',
 	'server' => 'tecnologiawebunid.com',
-	'username' => 'tecnol43_ealor',
-	'password' => 'ErickAlor1995&'
+	'username' => 'tecnol43_principal',
+	'password' => 'Principal.2018!'
 ]);
 ?>
--- maquetado/admin/includes/_funciones.php
+++ maquetado/admin/includes/_funciones.php
@@ -6,40 +6,85 @@ if(!isset($_POST['accion'])){
 	$accion = $_POST['accion'];
 }
 switch ($accion) {
-	case 'eliminarServicio': eliminarServicio();
-	break;
-	
-	case 'insertar' : insertar();
+	case 'eliminarUsuario': eliminarUsuarios();
 	break;
 
-	case 'consultar' : consultarServicio();
+	case 'eliminarSucursal': eliminarSucursales();
 	break;
 
-	default:
-		# code...
+	case 'eliminarServicio': eliminarServicios();
 	break;
+
+	case 'insertar': insertar();
+	break;
+
+	case 'individual' : individual();
+	break;
+
+	case 'editar' : editar();
+	break;
+}
+function consultarUsuarios(){
+	global $db;
+	$usuarios = $db->select("usuarios","*");
+	return $usuarios;
+}
+function consultarSucursales(){
+	global $db;
+	$sucursales = $db->select("sucursales","*");
+	return $sucursales;
+}
+function consultarServicios(){
+	global $db;
+	$servicios = $db->select("servicios","*");
+	return $servicios;
+}
+function eliminarUsuarios(){
+	global $db;
+	extract($_POST);
+	$usuarios = $db->delete("usuarios",["id_usr" => $usuario]);
+	echo "Se ha eliminado el usuario correctamente";	
+}
+function eliminarSucursales(){
+	global $db;
+	extract($_POST);
+	$sucursales = $db->delete("sucursales",["id_suc" => $sucursal]);
+	echo "Se ha eliminado la sucursal correctamente";	
+}
+function eliminarServicios(){
+	global $db;
+	extract($_POST);
+	$servicios = $db->select("servicios",["id_ser" => $servicio]);
+	echo "Se ha eliminado el servicio correctamente";
 }
 function insertar(){
 	global $db;
 	extract($_POST);
 	$db->insert("servicios",[
-		"sucursales" => $sucursales,
-		"experiencia" => $experiencia,
-		"clientes_satisfechos" => $clientes,
-		"empleados" => $empleados
+		"suc_ser" => $sucursales,
+		"exp_ser" => $exp,
+		"cl_ser" => $clientes,
+		"emp_ser" => $emp
 	]);
 	$servicio = $db->id();
 	echo "Se ha registrado correctamente el servicio con ID ".$servicio;
 }
-function consultarServicios(){
+function editar(){
 	global $db;
-	$servicio = $db->select("servicios","*");
-	echo json_encode($servicios);
+	print_r($_POST);
+	extract($_POST);
+	$db->update("servicios",[
+		"suc_ser" => $sucursales,
+		"exp_ser" => $exp,
+		"cl_ser" => $clientes,
+		"emp_ser" => $emp
+	], ["id_ser" => $id]);
+	echo "Se ha actualizado correctamente el servicio con ID ".$id;
 }
-function eliminarServicios(){
+function individual(){
 	global $db;
 	extract($_POST);
-	$servicio = $db->delete("servicios",["id_servicios" => $servicio]);
-	echo "Se ha eliminado el servicio correctamente";	
+	$servicios = $db->get("servicios","*",["id_ser" => $id]);
+	echo json_encode($servicios);
 }
 ?>
